5cee565bd00d1e91d74ef8bdfc72e0770c3ac233
darkllen <yankiniigor@gmail.com>
2021-04-04 20:32:20 +0300

    add method

--- src/main/java/project/db/controllers/RestController.java
+++ src/main/java/project/db/controllers/RestController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import project.db.dto.Product;
 import project.db.repos.CategoryRepo;
 import project.db.repos.CustomerRepo;
+import project.db.repos.ProductRepo;
 
 import java.util.List;
 
@@ -20,6 +23,9 @@ public class RestController {
     @Autowired
     private CustomerRepo customerRepo;
 
+    @Autowired
+    private ProductRepo productRepo;
+
     @RequestMapping(value = {"/get_categories_count"}, method = RequestMethod.GET)
     public ResponseEntity<List<CategoryRepo.CategoriesProductsCounted>> get_categories_count(){
         return ResponseEntity.status(HttpStatus.OK).body(categoryRepo.getCategoriesProductsCount());
@@ -31,9 +37,15 @@ public class RestController {
         return ResponseEntity.status(HttpStatus.OK).body(customerRepo.getCustomersSpends());
     }
 
+    @RequestMapping(value = {"/get_all_products_that_clients_from_city_buy"}, method = RequestMethod.GET)
+    public ResponseEntity<List<Product>> get_all_products_that_clients_from_city_buy(@RequestBody final String city){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepo.getAllProductsThatClientsFromCityNBuy(city));
+    }
+
+
     @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
     public String test(){
-        customerRepo.getCustomersSpends().forEach(x->System.out.println(x.getTotal()));
+        productRepo.getAllProductsThatClientsFromCityNBuy("K").forEach(System.out::println);
         return "index";
     }
 }
--- src/main/java/project/db/repos/ProductRepo.java
+++ src/main/java/project/db/repos/ProductRepo.java
@@ -2,15 +2,20 @@ package project.db.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.db.dto.Product;
 
 import java.util.List;
 
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
-    @Query(
-            value = "SELECT * " +
-                    "FROM Product",
-            nativeQuery = true)
-    List<Product> getAllProducts();
+
+    @Query(value = "SELECT  Product.id_product, category_number, product_name, characteristics " +
+            "FROM (Customer_Card INNER JOIN Recipe ON Customer_Card.card_number=Recipe.card_number" +
+            "                    INNER JOIN Sale ON Recipe.check_number=Sale.check_number" +
+            "                    INNER JOIN Store_Product ON Sale.UPC=Store_Product.UPC" +
+            "                    INNER JOIN Product ON Store_Product.id_product=Product.id_product) " +
+            "WHERE Customer_Card.city=:city", nativeQuery = true)
+    List<Product> getAllProductsThatClientsFromCityNBuy(@Param("city") String city);
+
 }
