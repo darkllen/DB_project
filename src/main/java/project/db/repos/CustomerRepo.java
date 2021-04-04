ad019beb931946ec9f915ab24d5e35dba4dc440f
darkllen <yankiniigor@gmail.com>
2021-04-04 19:56:20 +0300

    add method

---
+++ src/main/java/project/db/repos/CustomerRepo.java
@@ -0,0 +1,22 @@
+package project.db.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import project.db.dto.Category;
+import project.db.dto.Customer_Card;
+
+import java.util.List;
+
+public interface CustomerRepo extends JpaRepository<Customer_Card, String> {
+
+    @Query(value = "SELECT Customer_Card.card_number as card_number, cust_surname, cust_name, SUM(sum_total) as total " +
+            "FROM (Customer_Card INNER JOIN Recipe ON Customer_Card.card_number=Recipe.card_number) " +
+            "GROUP BY Customer_Card.card_number", nativeQuery = true)
+    List<CustomerSpends> getCustomersSpends();
+    interface CustomerSpends {
+        int getCard_number();
+        String getCust_surname();
+        String getCust_name();
+        double getTotal();
+    }
+}
