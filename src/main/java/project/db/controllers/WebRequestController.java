4898cd11f31678a469c806cc6c844ac3a26577df
Ana22102000 <45357776+Ana22102000@users.noreply.github.com>
2021-04-11 23:53:37 +0300

    double

---
+++ src/main/java/project/db/controllers/WebRequestController.java
@@ -0,0 +1,39 @@
+package project.db.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequiredArgsConstructor
+public class WebRequestController {
+
+    @RequestMapping(value = {"/category_requests}"}, method = RequestMethod.GET)
+    public String category(){
+        return "requests/category_requests";
+    }
+    @RequestMapping(value = {"/employee_requests}"}, method = RequestMethod.GET)
+    public String employee(){
+        return "requests/employee_requests";
+    }
+    @RequestMapping(value = {"/product_requests}"}, method = RequestMethod.GET)
+    public String product(){
+        return "requests/product_requests";
+    }
+    @RequestMapping(value = {"/customer_card_requests}"}, method = RequestMethod.GET)
+    public String customer_card(){
+        return "requests/customer_card_requests";
+    }
+    @RequestMapping(value = {"/store_product_requests}"}, method = RequestMethod.GET)
+    public String store_product(){
+        return "requests/store_product_requests";
+    }
+    @RequestMapping(value = {"/check_requests}"}, method = RequestMethod.GET)
+    public String check(){
+        return "requests/check_requests";
+    }
+
+}
